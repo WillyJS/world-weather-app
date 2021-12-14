b52bd12b1e51a0b65497fd263e4af9ad6e7f7f02
Will <williamjotatan@gmail.com>
2021-12-14 08:36:29 -0500

    Added new pictures

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " mp/h";
 
-        document.querySelector(".weather").classList.remove("loading");
+       // document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')"
     },
 
